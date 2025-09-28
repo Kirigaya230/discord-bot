8ece5b20e007fa9fe7e26f978f3bfc39658e1c03
Kirigaya230 <miguel13012005@gmail.com>
2025-09-28 13:14:03 -0500

    Actualizar la gestión de comandos y mejorar la estructura del bot

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const {Client, Collection, GatewayIntentBits, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, UserSelectMenuBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, PermissionsBitField, EmbedBuilder} = require('discord.js');
+const {Client, Collection, GatewayIntentBits, EmbedBuilder, ActionRowBuilder, StringSelectMenuBuilder, UserSelectMenuBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, PermissionsBitField} = require('discord.js');
 require('dotenv').config();
 
 console.log('🔁 Iniciando bot...');
